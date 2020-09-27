8ba4664d587271bd4f5941adab52dfe5cb7f38ed
sanjay suthar <sanjaysuthar661996@gmail.com>
2020-09-27 11:46:56 +0530

    PRLIB-2 slist.h : Refined API declaration and description for singly linked list
    Signed-off-by: sanjay suthar <sanjaysuthar661996@gmail.com>

--- include/slist.h
+++ include/slist.h
@@ -1,7 +1,6 @@
 #ifndef _S_LIST_H
 #define _S_LIST_H
 
-#include <stdlib.h>
 /*
  * copyright <TODO>
  */
@@ -48,31 +47,43 @@ int insert_node_in_end(slist_t *slist, void *data);
 /* @function : delete node in the front of a list.
  *
  * @param : pointer to list
- * @param : data to be inserted
+ *
  * @returns : 0 for success and -1 for failure
  * */
-int * delete_node_in_front(slist_t *list, void *data);
+int delete_node_in_front(slist_t *slist);
 
-/* @function : Insert node in the end of a list.
+/* @function : delete node in the end of a list.
  *
  * @param : pointer to list
- * @param : data to be inserted
+ *
  * @returns : 0 for success and -1 for failure
  * */
-int * delete_node_in_end(slist_t *list, void *data);
+int delete_node_in_end(slist_t *slist);
+
+/* @function : delete specific node in the list.
+ *
+ * NOTE : here it will only delete the first matching node
+ *
+ * @param : pointer to list
+ * @param : data to be deleted
+ *
+ * @returns : 0 for success and -1 for failure
+ * */
+int delete_specific_node(slist_t *list, void *data);
+
+/* @function : print the list.
+ *
+ * @param : pointer to list
+ * @returns : 0 for success and -1 for failure
+ * */
+int print_list(slist_t *slist);
+
 
 /* @function : print the list.
  *
  * @param : pointer to list
  * */
-void print_list(slist_t *list);
-
-
-/* @function : print the list.
- *
- * @param : pointer to list
- * */
-int free_list(slist_t *list);
+int free_list(slist_t *slist);
 
 
 
